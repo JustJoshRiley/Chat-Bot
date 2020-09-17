5060e1c40c8f3d8802452eab12192238163ed965
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-09-17 12:38:47 -0700

    first commit

---
+++ chat-bot.py
@@ -0,0 +1,10 @@
+# This will give you access to the random module or library.
+from random import choice
+
+#make an array for computer.
+computer = ["Rock", "Paper", "Scissors"]
+#make an array for losing the game.
+lost = ["Rock Crushes Scissors! You lose Mate!", "Paper Got Sliced By Scissors! Thats gotta hurt!", "Rock Got Smothered By Paper!, Time for you to go to bed!"]
+#make an array for winning a game.
+wins = ["Rock Crushes Scissor!, Way to go bro!", "Paper got cut by Scissors!, You're on a roll!", "Paper Smothered the Rock!, You are unstopable!"]
+
