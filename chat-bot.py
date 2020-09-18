e72d8dc2598d6dce6588a03391061e1bf6d48f38
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-09-17 18:57:28 -0700

    fixed input responses

--- chat-bot.py
+++ chat-bot.py
@@ -1,11 +1,11 @@
 # import random
 from random import choice, randint
 #Make Greeting
-print("Hey There! Welcome to Rock Paper Scissors. You will be Demolished by the Chat Bot you are playing against...Failure is inevitable.")
+print("Hey There! Welcome to Rock, Paper, Knife. You will be Demolished by the Chat Bot you are playing against...Failure is inevitable.")
 
 def get_bot_response(user_response):
     #make an array for computer.
-    computer = ["Rock", "Paper", "Scissors"]
+    computer = ["Rock", "Paper", "Knife"]
     #make an array for losing the game.
     player_lost = ["You lose Mate!", "Ouch! You put the LOSE in LOSER! Thats gotta hurt!", "You Lost! Time for you to go to bed!"]
     #make an array for winning a game.
@@ -22,37 +22,46 @@ def get_bot_response(user_response):
         print(computer_choice)
         if computer_choice == player:
             print("Draw!")
+
+        # Paper versus Rock
+        elif computer_choice == "Paper" and player == "Rock":
+            # if player == "Rock":
+            print(choice(player_lost))
+
+        #Paper Versus Knife
+        elif computer_choice == "Paper" and player == "Knife":
+            # if player == "Knife":
+            print(choice(player_wins))
+
+        # Rock versus Paper
+        elif computer_choice == "Rock" and player == "Paper":
+            # if player == "Paper":
+            print(choice(player_wins))
+
+        ######Rock Versus Knife not working
+        elif computer_choice == "Rock" and player == "Knife":
+            # if player == "Knife":
+            print(choice(player_lost))
         
-        elif computer_choice == "Paper":
-            if player == "Rock":
-                print(choice(player_lost))
-            else: #computer is Rock, player is Paper
-                print(choice(player_wins))
-        elif computer_choice == "Scissors":
-            if player == "Rock":
-                print(choice(player_wins))
-            else: #computer is Scissors, player is Paper
-                print(choice(player_lost))
-        elif computer == "Rock":
-            if player == "Scissors":
-                print(choice(player_lost))
-            else:
-                print(choice(player_wins))
-        # else:
-        #     play_again = input("Would you like to play again? (yes/no) > ")
-        #     if play_again == 'yes':
-        #         player = False
-        #         computer_choice = computer[randint(0,2)]
-        else:
-            break
+        #Knife versus Rock
+        elif computer_choice == "Knife" and player == "Rock":
+            # if player == "Rock":
+            print(choice(player_wins))
+
+        ####Knife versus Paper
+        elif computer_choice == "Knife" and player == "Paper":
+            # if player == "Paper":
+            print(choice(player_lost))
+
+        
+    # else:
 
 
 
 
-user_response = False
-while user_response == False:
-    user_response = input("Choose Rock, Paper, or Scissors: ")
 
 
-run_game = get_bot_response(user_response)
+
+# computer = get_bot_response(user_response)
+# print(run_game)
 
